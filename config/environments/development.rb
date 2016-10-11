47e1515938696bf00383c9b732c7bd5f9d35eeb7
Dustin Milam <darklordsushi@yahoo.com>
2016-10-11 10:48:24 -0400

    Updated gemfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Renders an HTML page with debugging information
+  config.debug_exception_response_format = :default
 end
